7f28cd143dee6131453f9481b2fe0f683617c98d
Igor <nekto.korobko@gmail.com>
2023-03-10 01:33:44 +0300

    create functions get_comics_data, fetch_comics_image

--- xkcd.py
+++ xkcd.py
@@ -1,4 +1,26 @@
+import os
+
 import requests
+
+
+def get_comics_data(comics_id):
+    response = requests.get(f'https://xkcd.com/{comics_id}/info.0.json')
+    response.raise_for_status()
+
+    return response.json()
+
+
+def fetch_comics_image(comics_data):
+    title = comics_data['title']
+    image_url = comics_data['img']
+    filename = os.path.basename(image_url)
+
+    response = requests.get(image_url)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+    return filename, title
 
 
 def get_last_id_comics() -> int:
