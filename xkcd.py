8d97bff88a6fa02146926751f048717db62e0eb9
Igor <nekto.korobko@gmail.com>
2023-03-11 17:28:15 +0300

    change name function get_comics_data to get_comics_metadata and get_last_id_comics to get_last_comics_id reduce variable in fetch_comics_image

--- xkcd.py
+++ xkcd.py
@@ -3,16 +3,14 @@ import os
 import requests
 
 
-def get_comics_data(comics_id):
+def get_comics_metadata(comics_id):
     response = requests.get(f'https://xkcd.com/{comics_id}/info.0.json')
     response.raise_for_status()
 
     return response.json()
 
 
-def fetch_comics_image(comics_data):
-    title = comics_data['title']
-    image_url = comics_data['img']
+def fetch_comics_image(image_url):
     filename = os.path.basename(image_url)
 
     response = requests.get(image_url)
@@ -20,10 +18,10 @@ def fetch_comics_image(comics_data):
 
     with open(filename, 'wb') as file:
         file.write(response.content)
-    return filename, title
+    return filename
 
 
-def get_last_id_comics() -> int:
+def get_last_comics_id() -> int:
     response = requests.get("https://xkcd.com/info.0.json")
     response.raise_for_status()
 
